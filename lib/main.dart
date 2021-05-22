082ba829c097c2b039280ceda1c75e39a29c0aa0
Papa Daco <papadaco@PapaDaco.local>
2021-05-22 10:55:11 -1000

    CFADB I Am Poor Challenge - I am Illidank

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Illidank',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('I am Illidank'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/images/i-am-illidank.png'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Dank',
+        child: Icon(Icons.fireplace),
+      ),
+    );
+  }
+}
